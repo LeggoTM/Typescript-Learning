20447e347e712fb7825f4de9546963326c4571c4
LeggoTM <m.tanmay17@gmail.com>
2024-03-04 17:57:38 +0530

    Add Datatypes

---
+++ dataTypes.ts
@@ -0,0 +1,35 @@
+// Boolean
+let isDone: boolean = true;
+let isEnabled: boolean = false;
+
+
+console.log(isDone);
+console.log(isEnabled);
+
+// Number
+let num1: number = 17;
+let num2: number = 17.45;
+
+console.log(num1);
+console.log(num2);
+
+// String
+let myName: string = "Tanmay";
+let lastName: string = "Playwright";
+
+console.log(myName);
+console.log(lastName);
+
+// Any type
+let dynamicValue: any = 42;
+console.log(dynamicValue);
+dynamicValue = "A String";
+console.log(dynamicValue);
+
+
+// Null and Undefined
+let nullValue: null = null;
+let undefinedValue: undefined = undefined;
+console.log(nullValue);
+console.log(undefinedValue);
+
